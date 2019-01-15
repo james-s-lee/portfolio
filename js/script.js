b6eef031dd2984d02725493b47d808766cc6818d
James Lee <41328459+james-s-lee@users.noreply.github.com>
2019-01-15 19:56:21 +0900

    Update file via upload

--- js/script.js
+++ js/script.js
@@ -13,6 +13,60 @@ $(document).ready(function() {
       startDelay: 1000,
       showCursor: false,
 
-    })
+    });
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        //margin:10,
+        //nav:true,
+        items: 4,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+
+    var skillsTopOffset = $(".skillsSection").offset().top;
+    var statsTopOffset = $(".statsSection").offset().top;
+    var countUpFinished = false;
+
+    $(window).scroll(function() {
+      if(window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+        $('.chart').easyPieChart({
+          easing: 'easeInOut',
+          barColor: '#fff',
+          trackColor: false,
+          scaleColor: false,
+          linewidth: 4,
+          size: 152,
+          animate: {
+            duration: 2000,
+            enabled: true
+          },
+          onStep: function(from, to, percent) {
+            $(this.el).find('.percent').text(Math.round(percent));
+          }
+        });
+      }
+      if(!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
+        $(".counter").each(function() {
+          var element = $(this);
+          var endVal = parseInt(element.text());
+
+          element.countup(endVal);
+        })
+
+        countUpFinished = true;
+      }
+
+    });
+
 
 });
