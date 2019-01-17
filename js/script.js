947e848fe1f1be7775649c31c9a9af4e74b33de1
James Lee <41328459+james-s-lee@users.noreply.github.com>
2019-01-18 03:43:42 +0900

    Update files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+$(window).on("load", function() {
+  $(".loader .inner").fadeOut(500, function() {
+    $(".loader").fadeOut(750);
+  });
+  $(".items").isotope({
+    filter: '*',
+    animationOptions: {
+      duration: 1500,
+      easing: 'linear',
+      queue: false
+    }
+  });
+})
+
 $(document).ready(function() {
 
     $('#slides').superslides({
@@ -16,6 +30,8 @@ $(document).ready(function() {
     });
 
     $('.owl-carousel').owlCarousel({
+        autoplay: true,
+        autpplayTimeout: 1000,
         loop:true,
         //margin:10,
         //nav:true,
@@ -68,5 +84,54 @@ $(document).ready(function() {
 
     });
 
+    $("[data-fancybox]").fancybox();
+
+
+
+    $("#filters a").click(function() {
+
+      $("#filters .current").removeClass("current");
+      $(this).addClass("current");
+
+      var selector = $(this).attr("data-filter");
+
+      $(".items").isotope({
+        filter: selector,
+        animationOptions: {
+          duration: 1500,
+          easing: 'linear',
+          queue: false
+        }
+      });
+
+      return false;
+    });
+
+    $("#navigation li a").click(function(e) {
+      e.preventDefault();
+
+      var targetElement = $(this).attr("href");
+      var targetPosition = $(targetElement).offset().top;
+      $("html, body").animate({ scrollTop: targetPosition - 50 }, "slow")
+    });
+
+    const nav = $("#navigation");
+    const navTop = nav.offset().top;
+
+    $(window).on("scroll", stickyNavigation);
+
+    function stickyNavigation() {
+
+      var body = $("body");
+
+      if($(window).scrollTop() >= navTop) {
+        body.css("padding-top", nav.outerHeight() + "px");
+        body.addClass("fixedNav");
+      }
+      else {
+        body.css("padding-top", 0);
+        body.removeClass("fixedNav");
+      }
+    }
 
 });
